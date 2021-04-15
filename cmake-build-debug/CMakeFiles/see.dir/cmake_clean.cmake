b138fa01e8411d72b1b2c68fded0850d2b2a42ce
Otto Tovisi <tovisiotto@gmail.com>
2021-04-15 17:40:55 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/see.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/see.dir/main.c.obj"
+  "libsee.dll.a"
+  "see.exe"
+  "see.exe.manifest"
+  "see.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/see.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
